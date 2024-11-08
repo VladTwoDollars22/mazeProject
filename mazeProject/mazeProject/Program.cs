ed0fb24160dc91a74e4cf907f2b8b2c0eccb2752
VladTwoDollars22 <burkenavladislav@gmail.com>
2024-11-08 22:37:06 +0200

    Writing a main functions and general mechanic(finish)

--- mazeProject/mazeProject/Program.cs
+++ mazeProject/mazeProject/Program.cs
@@ -4,6 +4,8 @@
 //Собака
 //Керування
 //Вихід
+using System.Runtime.CompilerServices;
+
 class Program
 {
     static Random random = new Random();
@@ -12,20 +14,30 @@ class Program
     static char[,] field;
     static int width = 10;
     static int height = 12;
-
     static int block_freq = 28;
 
     //dog
     static char dog = '@';
-    static int dogX = 0;static int dogY = 0;
+    static int dogX = 0;
+    static int dogY = 0;
+    static int dogSpeed = 1;
 
     //input
     static int dx = 0;static int dy = 0;
 
     //finish
-    static int finishX = 0;static int finishY = 0;
-
+    static int finishX = 0;
+    static int finishY = 0;
     static bool reached_finish = false;
+
+    //JetPack
+    static bool jetpackAvalaible = false;
+    static bool jetpackIsActive = false;
+    static int jetpackX = 0;
+    static int jetpackY = 0;
+    static int jetpackSpeed = 2;
+    static int jetpackTimer = 0;
+
     static void generate_field()
     {
         field = new char[height, width];
@@ -43,9 +55,6 @@ class Program
                 field[i, j] = symbol;
             }
         }
-        finishX = random.Next(0, width - 1);
-        finishY = random.Next(0, height - 1);
-        field[finishY, finishX] = 'F';
     }
 
     static void draw()
@@ -65,37 +74,106 @@ class Program
         }
     }
 
+    static void PlaceJetPack()
+    {
+        var point = GetRandomPoint();
+
+        jetpackX = point.X;
+        jetpackY = point.Y;
+
+        field[jetpackY, jetpackX] = 'J';
+    }
+    static void PlaceFinish()
+    {
+        var point = GetRandomPoint();
+
+        finishX = point.X;
+        finishY = point.Y;
+
+        field[finishY, finishX] = 'F';
+    }
+
     static void place_dog()
     {
-        dogX = random.Next(0, width - 1);
-        dogY = random.Next(0, height - 1);
+        var point = GetRandomPoint();
+
+        dogX = point.X;
+        dogY = point.Y;
+    }
+
+    static (int X,int Y) GetRandomPoint()
+    {
+        int pointX = random.Next(0, width - 1);
+        int pointY = random.Next(0, height- 1);
+
+        return (pointX, pointY);
     }
 
     static void generate()
     {
         generate_field();
+        PlaceFinish();
+        PlaceJetPack();
         place_dog();
     }
 
-    static (int dx, int dy) get_input()
+    static void get_input()
     {
         dx = 0; dy = 0;
         string inp = Console.ReadLine();
         if (inp.Length == 0)
-            return (0, 0);
+            return;
 
         char first_symbol = inp[0];
 
-        if (first_symbol == 'W' || first_symbol == 'w')
-            dy = -1;
-        if (first_symbol == 'A' || first_symbol == 'a')
-            dx = -1;
-        if (first_symbol == 'S' || first_symbol == 's')
-            dy = 1;
-        if (first_symbol == 'D' || first_symbol == 'd')
-            dx = 1;
+        CheckActionsInput(first_symbol);
+        CheckMovementInput(first_symbol);
+    }
 
-        return (dx, dy);
+    static void CheckActionsInput(char symbol)
+    {
+        if (symbol == 'J' || symbol == 'j' && jetpackAvalaible)
+        {
+            jetpackIsActive = true;
+            jetpackTimer = 3;
+            jetpackAvalaible = false;
+            return;
+        }
+    }
+
+    static void CheckMovementInput(char symbol)
+    {
+        switch (symbol)
+        {
+            case 'W' or 'w':
+                dy = -dogSpeed;
+                break;
+            case 'A' or 'a':
+                dx = -dogSpeed;
+                break;
+            case 'S' or 's':
+                dy = dogSpeed;
+                break;
+            case 'D' or 'd':
+                dx = dogSpeed;
+                break;
+
+        }
+    }
+
+    static void JetpackLogic()
+    {
+        jetpackTimer--;
+        if (jetpackTimer == 0)
+        {
+            jetpackIsActive = false;
+            return;
+        }
+        if (jetpackIsActive == true)
+        {
+            dx *= jetpackSpeed;
+            dy *= jetpackSpeed;
+        }
     }
     static bool can_go_to(int newX, int newY)
     {
@@ -122,9 +200,21 @@ class Program
         }
     }
 
+    static void CheckJetPack()
+    {
+        if(dogX == jetpackX && dogY == jetpackY)
+        {
+            jetpackAvalaible = true;
+
+            field[jetpackY, jetpackX] = '.';
+
+            PlaceJetPack();
+        }
+    }
+
     static void check_finish()
     {
-        if(dogX == finishX && dogY == finishY)
+        if (dogX == finishX && dogY == finishY)
         {
             reached_finish = true;
         }
@@ -144,9 +234,10 @@ class Program
 
     static void logic()
     {
+        JetpackLogic();
         try_go_to(dogX + dx,dogY + dy);
-
         check_finish();
+        CheckJetPack();
     }
 
     static void Main(string[] args)
@@ -156,7 +247,7 @@ class Program
 
         while (!is_end_game())
         {
-            var input = get_input();dx = input.dx;dy = input.dy;
+            get_input();
             logic();
             draw();
         }
